a18f587f680d3fb90116df3efbf8d0b41f021f21
Evgenii Biktimirkin <biktizhenya997@mail.ru>
2026-02-17 18:19:20 +0400

    add: slider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validator from "./modules/validator";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer("15 february 2026");
 menu();
 modal();
 validator();
 tabs();
+slider();
